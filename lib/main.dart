1407ba276e82765d9beb4a261fdf85b72049f697
Man Nok Pun <sgloriasca@gmail.com>
2023-01-10 19:35:11 -0500

    Just display name and student number

--- lib/main.dart
+++ lib/main.dart
@@ -96,20 +96,11 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+              'Man Nok Pun (301269138)',
+            )
+            
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+      ) );
   }
 }
