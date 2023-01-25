73025c7a2503b2d8fc5cea21374668c4c9026e8d
Man Nok Pun <sgloriasca@gmail.com>
2023-01-25 10:51:21 -0500

    Calculator program

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:math_expressions/math_expressions.dart';
+import './buttons.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -48,59 +49,134 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  var userInput = 'text';
+  var answer = '';
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  final List<String> buttons = [
+    'C',
+    '(',
+    ')',
+    'DEL',
+    '7',
+    '8',
+    '9',
+    '/',
+    '4',
+    '5',
+    '6',
+    '*',
+    '1',
+    '2',
+    '3',
+    '-',
+    '0',
+    '.',
+    '=',
+    '+',
+  ];
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      appBar: new AppBar(
+        title: new Text('Calculator'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Man Nok Pun (301269138)',
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              color: Colors.grey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(20),
+                    alignment: Alignment.centerRight,
+                    child: Text( 
+                      userInput, 
+                      style: TextStyle(fontSize: 18, color: Colors.white)
+                    )
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(20),
+                    alignment: Alignment.centerRight,
+                    child: Text( 
+                      answer, 
+                      style: TextStyle(fontSize: 28, color: Colors.white)
+                    )
+                  )
+                ]
+              )
             )
-            
-          ],
-        ),
-      ) );
+          ), 
+          Expanded(
+            flex: 3,
+            child: GridView.builder(
+              itemCount: buttons.length,
+              gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 4
+              ),
+              itemBuilder: (BuildContext context, int index) {
+                if(index == 0) {
+                  return MyButton(
+                    buttonTapped:(){
+                      setState((){
+                        userInput = '';
+                        answer = '0';
+                      });
+                    },
+                    buttonText: buttons[index],
+                    color: Colors.red[50],
+                    textColor: Colors.black
+                  );
+                } else if(index == 3) {
+                  return MyButton(
+                    buttonTapped:(){
+                     setState((){  userInput = userInput.substring(0, userInput.length - 1); });
+                    },
+                    buttonText: buttons[index],
+                    color: Colors.blue[50],
+                    textColor: Colors.black
+                  );
+                } else if(index == 18) {
+                  return MyButton(
+                    buttonTapped:(){
+                      setState((){ equalPressed(); });
+                    },
+                    buttonText: buttons[index],
+                    color: Colors.orange[50],
+                    textColor: Colors.black
+                  );
+                } else {
+                  return MyButton(
+                    buttonTapped:(){
+                      setState((){  userInput = userInput + buttons[index]; });
+                    },
+                    buttonText: buttons[index],
+                    color: (isOperator(buttons[index]) ? Colors.blue[50] : Colors.white),
+                    textColor: Colors.black
+                  );
+                }
+              },
+            )
+          )
+        ],
+      ),
+    );
+  }
+
+  void equalPressed(){
+    String finalInput = userInput; 
+
+    Parser p = Parser();
+    Expression exp = p.parse(finalInput);
+    ContextModel cm = ContextModel();
+    double eval = exp.evaluate(EvaluationType.REAL, cm);
+    answer = eval.toString();
+  } 
+  bool isOperator(str) {
+    if(str == '+' || str == '-' || str == '*' || str == '/' || str == '(' || str == ')') {
+      return true; 
+    } else {return false; }
   }
 }
